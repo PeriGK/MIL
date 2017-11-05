57b9a08965e92a299266d0aa1035725eafeb05f6
periklis <per.gkolias@gmail.com>
2017-11-05 22:44:09 +0200

    Adding some initial configuration to the project

---
+++ conf/mil_conf.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+database = {'host': 'localhost',
+         'username': 'root',
+         'password': 'my secret password',
+         'db_name': 'mil'}
+
+log_dir = './'
+log_file = 'mil.out'
